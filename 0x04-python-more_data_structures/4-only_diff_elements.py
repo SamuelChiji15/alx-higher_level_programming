68c0423b9170a4e1169f3a21ab193c6604a3f204
SamuelChiji15 <samuelchijioke1999@gmail.com>
2023-01-23 05:18:20 -0800

    task all

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -79,7 +79,10 @@
 ~
 ~
 -- INSERT --                                                          0,1           All
-~
+#!/usr/bin/python3
+
+def only_diff_elements(set_1, set_2):
+    return (set_1.union(set_2).difference(set_1 & set_2))~
 ~
 ~
 ~
